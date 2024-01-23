90e57988552450656f47ed5f59e1fb8662fd7646
Sophia Zhang <sophiasz2004@gmail.com>
2024-01-23 15:53:07 -0800

    Working on calibrator for vehicle speed

--- tools/joystick/joystick_copy.py
+++ tools/joystick/joystick_copy.py
@@ -9,12 +9,15 @@ from openpilot.common.params import Params
 import argparse
 import os
 
+# vEgo imports - check line 274 in the openpilot file for ex
+from selfdrive.locationd.calibrationd import *
+
 
 class datastream:
     def __init__(self, csv_filepath):
         """Set default values for when datastream object is created. Incr / decr only set to 1%"""
-        self.axis_increment = 0.01    # 1% incr / decr when adjusting speed to match
-        self.axis_values = {'gb':0., 'steer': 0.} 
+        self.axis_increment = 0.01    # 1% incr / decr to adjusting speed
+        self.axis_values = {'gb':0.01, 'steer': 0.}   # initialize gb to a small value for later on proportional calculation
         self.axes_order = ['gb', 'steer']
         self.csv_filepath = csv_filepath  
         self.control_sock = messaging.pub_sock('testJoystick')
@@ -36,16 +39,13 @@ class datastream:
                 self.speed_list.append(float(row['speed']))
         return data
 
+
     def get_target_speed(self, elapsed_time):
         """
         Return target speed based on the elapsed time.
         """
-        # for row in self.csv_data:
-        #     if float(row['time']) >= elapsed_time:
-        #         return float(row['speed'])
-        # If elapsed_time exceeds the data range, return the last speed (this case should not happen based on update_speed)
-        # return float(self.csv_data[-1]['speed'])
         return interp(elapsed_time, self.time_list, self.speed_list)  # interp return type should be a float
+
 
     def control_speed(self, start_time):
         """
@@ -53,26 +53,12 @@ class datastream:
         """
         # loop through the csv rows and update speed based on P controller
         # load csv data into a list of dictionaries
-        # CSV could look like this
-        # time,speed
-        # 0,0
-        # 1,5
-        # 2,10
-        # 3,15
-        # 4,20
-        # List of Dictionaries could look like this
-        # [{'time': 0, 'speed': 0}
-        # {'time': 1, 'speed': 5}
-        # {'time': 2, 'speed': 10}
-        # {'time': 3, 'speed': 15}
-        # {'time': 4, 'speed': 20}]
 
         # calculate elapsed time 
         end_time = self.time_list[-1]
         current_time = time.time()
         elapsed_time = current_time - start_time
         while elapsed_time < end_time:   # should this be <=?
-            #elapsed_time = current_time - start_time   
 
             # Get the target speed based on the elapsed time from CSV data
             target_speed = self.get_target_speed(elapsed_time)
@@ -80,9 +66,23 @@ class datastream:
             # Proportional controller or other logic to adjust the speed (need to implement)
             error = target_speed - self.axis_values['gb']   # need to get the actual GB value from openpilot for car
             gas_brake = clip(self.axis_values['gb'] + error, -1, 1)  # with 0.1 being the tuning parameter
-            # clip means the value must be [-1, 1]
+            # clip to ensure the value in [-1, 1]
+            self.axis_values['gb'] = gas_brake  # set the gb to calculated p
 
-            self.axis_values['gb'] = gas_brake  # set the gb to the calculated p
+
+            # use equation (prev_gas(curr_time - prev_time)) / (curr_target_V - prev_vEgo_V) 
+            # where curr refers to time at 1st second and prev refers to time at 0th second
+            prev_gas = self.axis_values['gb']
+            prev_time = elapsed_time - 1   # where elasped time refers to curr time
+            curr_target_V = self.get_target_speed(elapsed_time)
+            # prev_vEgo_V was calculated for the previous iteration
+
+            # set gb to the newly calculated proportion
+            self.axis_values['gb'] = (prev_gas * (elapsed_time - prev_time)) / (curr_target_V - prev_vEgo_V)
+            
+            # this calculation is used for the next iteration
+            calibrator.vEgo = sm['carState'].vEgo
+            prev_vEgo_V = calibrator.v_ego
 
             # send control signals to Openpilot
             # messaging communicates info between different components of the system
@@ -90,24 +90,12 @@ class datastream:
             dat.testJoystick.axes = [self.axis_values[a] for a in self.axes_order]  # put all values in axis_values into a list
             dat.testJoystick.buttons = [False]
             self.control_sock.send(dat.to_bytes())  # convert message to bytes and send it over messaging socket
-# to test this:
-# use print statements to see the changes in self.axes_values over time
-
-# how does this integrated with openpilo t system to update the car speed?
 
 
-
-            # Check if elapsed time has exceeded the last time in the CSV, if so exit loop
-            # if elapsed_time >= float(self.csv_data[-1]['time']):
-            #     break
-            print("target speed is: ", target_speed, "actual gb: ", self.axis_values['gb'])
-            time.sleep(0.01)
+            print("target speed: ", target_speed, "---- actual gb: ", self.axis_values['gb'])
+            time.sleep(0.01)  # adjustmnet occurs every 10ms, adjust if needed
             current_time = time.time()
             elapsed_time = current_time - start_time
-        #process_time = new_time - current_time
-
-        # Sleep for a short duration (not sure what to adjust this to)
-        #time.sleep(1 - process_time)   # make it more precise
 
 
 
@@ -125,6 +113,11 @@ if __name__ == '__main__':
     # create the data stream object
     ds = datastream(csv_filepath = args.csv_file)
     start_time = time.time() 
+    
+    #subMaster
+    sm = messaging.SubMaster(['cameraOdometry', 'carState', 'carParams'], poll=['cameraOdometry'])
+    # create calibrator object to get vEgo
+    calibrator = Calibrator(param_put=True)
 
     while True:
         ds.control_speed(start_time)
